e50f2e186c7d4a0f0cfbc0aa747300fd88ebb95f
miles-franklin-wright <67435684+miles-franklin-wright@users.noreply.github.com>
2021-08-01 18:56:27 -0400

    Add files via upload

---
+++ august_1st_2021.py
@@ -0,0 +1,31 @@
+# Good morning! Here's your coding interview problem for today.
+
+# This problem was recently asked by Google.
+
+# Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
+
+# For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
+
+# Bonus: Can you do this in one pass?
+
+
+x = [10, 15, 3, 7]
+y = []
+z = 0
+k = 17
+
+def trial(x, y):
+    f = []
+    for n in x:
+        a = n
+        for n in x:
+            z = a + n
+            if z == (k):
+                f.append(a)
+                f.append(n)
+    print(f)
+    
+    
+
+trial(x, y)
+
